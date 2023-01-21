f3242012316dcd8b4fcf5107611025cad726b595
MishynaIra <gortenzia1987@gmail.com>
2023-01-21 15:36:58 +0200

    all components are fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -93,6 +93,8 @@
 import { useEffect, useState } from 'react';
 import ContactForm from './ContactForm/ContactForm ';
 import { nanoid } from 'nanoid';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
 
 export const App =() => {
   const [filter, setFilter] = useState('');
@@ -133,11 +135,33 @@ export const App =() => {
     }
   }
 
+  const changeFilter = event => {
+    setFilter(event.currentTarget.value);
+  };
+
+  const getFilterContact = () => {
+    const normalizeFilter = filter.toLowerCase();
+
+    return contacts.filter(contact => contact.name.toLowerCase().includes(normalizeFilter));
+  };
+
+  const filterContact = () => {
+    return (
+      filter ? getFilterContact() : contacts
+    )
+  }
+
+  const handleDeleteContact = (id) => {
+    setContacts(prevState => (prevState.filter(contact => contact.id !== id)));
+  }
+
   return (
     <>
       <h1>Phonebook</h1>
-        <ContactForm addContact={addForm} />
-        <h2>Contacts</h2>
+      <ContactForm addContact={addForm} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      <ContactList contacts={filterContact()} onDelete={handleDeleteContact}/>
     </>
   )
 }
